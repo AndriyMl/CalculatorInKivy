67c7d71d76beb6eb11aa8bacb666a19ec587c4c2
AndriyMl <138151412+AndriyMl@users.noreply.github.com>
2025-11-15 19:46:49 +0200

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -9,11 +9,12 @@ class MyGridLayout(GridLayout):
         for w in widgets:
             self.add_widget(w)
 
+# Special keys can only be C, CE, D and =. All others will be treated as symbols or operands.
 buttons = ["C","CE","D","/",
            "1","2","3","*",
            "4","5","6","-",
            "7","8","9","+",
-           ",","0","%","="]
+           ".","0","%","="]
 
 font_size = 25
 
@@ -23,26 +24,26 @@ class MyApp(App):
         layout = BoxLayout(orientation='vertical', padding=5)
         LayoutGrid = MyGridLayout(cols=4, size_hint=(1, .75))
         self.label = Label(text="",size_hint=(1, 0.25), font_size=40,valign="center", halign="right", text_size=(.40,.50))
-        LayoutGrid.add_widgets(Button(text=str(buttons[0]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.cleaning),
-                               Button(text=str(buttons[1]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.cleaning_element),
-                               Button(text=str(buttons[2]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.delete_number_or_operant),
-                               Button(text=str(buttons[3]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[4]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[5]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[6]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[7]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[8]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[9]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[10]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[11]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[12]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[13]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[14]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[15]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[16]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[17]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[18]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.write_number_or_operant),
-                               Button(text=str(buttons[19]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.equal))
+        LayoutGrid.add_widgets(Button(text=str(buttons[0]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[1]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[2]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[3]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[4]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[5]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[6]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[7]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[8]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[9]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[10]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[11]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[12]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[13]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[14]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[15]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[16]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[17]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[18]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant),
+                               Button(text=str(buttons[19]),font_size=font_size,size_hint=(1/4, 1/5),on_press=self.operant))
 
         self.label.bind(size=lambda inst, val: setattr(inst, "text_size", inst.size))
         layout.add_widget(self.label)
@@ -50,31 +51,28 @@ class MyApp(App):
 
         return layout
 
-    def write_number_or_operant(self, instance):
-        self.label.text += instance.text
-
-    def delete_number_or_operant(self, instance):
-        self.label.text = self.label.text[:-1]
-
-    def equal(self, instance):
-        expr = self.label.text.replace(',', '.')
-        try:
-            self.label.text = str(eval(expr))
-        except Exception:
-            self.label.text = "Error"
-
-    def cleaning(self,instance):
-        self.label.text = ""
-
-    def cleaning_element(self, instance):
-        expr = self.label.text
-        for sep in ["/", "*", "-", "+"]:
-            expr = expr.replace(sep, f" {sep} ")
-        parts = expr.split()
-        if parts:
-            parts.pop()
-            self.label.text = "".join(parts)
-        else:
+    def operant(self,instance):
+        if instance.text == "D":
+            self.label.text = self.label.text[:-1]
+        elif instance.text == "=":
+            expr = self.label.text.replace(',', '.')
+            try:
+                self.label.text = str(eval(expr))
+            except Exception:
+                self.label.text = "Error"
+        elif instance.text == "C":
             self.label.text = ""
+        elif instance.text == "CE":
+            expr = self.label.text
+            for sep in ["/", "*", "-", "+"]:
+                expr = expr.replace(sep, f" {sep} ")
+            parts = expr.split()
+            if parts:
+                parts.pop()
+                self.label.text = "".join(parts)
+            else:
+                self.label.text = ""
+        else:
+            self.label.text += instance.text
 
 MyApp().run()
